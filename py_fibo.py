08d1ef93420dd3f546e509dc2e3a0a5d8885035a
makkalot <makkalot@gmail.com>
2008-08-25 20:23:14 +0300

    add the recursive Python vesion of the fibonaccii

--- py_fibo.py
+++ py_fibo.py
@@ -16,7 +16,29 @@ def compute_fibo(which_fibo):
 
     return second
 
+def compute_fibo_rec(fib_num):
+    
+    if fib_num == 0 or fib_num == 1:
+        return fib_num
+
+    return compute_fibo_rec(fib_num-1) +compute_fibo_rec(fib_num-2)
+
 if __name__ == "__main__":
     import sys
-    print compute_fibo(int(sys.argv[1]))
+    """
+    result = compute_fibo(int(sys.argv[1]))
+    f = open("fibo.txt","w")
+    f.write(str(result))
+    f.close()
+    print "Result written into fibo.txt"
+    """
 
+    result = compute_fibo_rec(int(sys.argv[1]))
+    print "The result is : ",result
+    sys.exit(0)
+    f = open("fibo.txt","w")
+    f.write(str(result))
+    f.close()
+    print "Result written into fibo.txt"
+
+    
