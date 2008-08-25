77e4943e0d03b7b96cf65381923ecf8a572df185
makkalot <makkalot@gmail.com>
2008-08-25 20:24:43 +0300

    the Py extension for recursive fibo and some fixes

--- ext_algo/fibo/ex_fibo.py
+++ ext_algo/fibo/ex_fibo.py
@@ -2,5 +2,18 @@ import fibo
 import sys
 
 if __name__ == "__main__":
-    print fibo.iter_fibo(int(sys.argv[1]))
-
+    
+    if sys.argv[1] == "iter":
+        result =  fibo.iter_fibo(int(sys.argv[2]))
+    else :
+        result =  fibo.rec_fibo(int(sys.argv[2]))
+        print "The recursive result is :",result
+        sys.exit(0)
+    
+    f = open("fibo.txt","w")
+    #print "The type of the result is : ",type(result)
+    #print "The type of the result is : ",result.__repr__()
+    f.write(str(result))
+    f.close()
+    print "Result written into fibo.txt"
+    
--- ext_algo/fibo/fibo.c
+++ ext_algo/fibo/fibo.c
@@ -16,13 +16,19 @@ fibo_iter(PyObject* self,PyObject *args){
     if (!PyArg_ParseTuple(args, "l", &fibo_number))
              return NULL;
 
+    //printf("\nThe value to compute is %ld",fibo_number);
 
     if(fibo_number == 0 || fibo_number == 1)
-        return Py_BuildValue("K",fibo_number);
+        return Py_BuildValue("l",fibo_number);
 
     //Dont forget to decref ...
-    first_num = Py_BuildValue("K",0);
-    second_num = Py_BuildValue("K",1);
+    first_num = Py_BuildValue("l",0);
+    second_num = Py_BuildValue("l",1);
+
+    //PyObject_Print(first_num, stdout, 0);
+    //printf("\n\n");
+    //PyObject_Print(second_num, stdout, 0);
+    //printf("\n\n");
 
     //computes the fibo iteratively
     for (i=1;i<fibo_number;i++){
@@ -45,9 +51,34 @@ fibo_iter(PyObject* self,PyObject *args){
     return second_num;
 }
 
+long int r_fib(long int f_num){
+    // A recursive implementation for that
+
+    if(f_num == 0 || f_num == 1)
+        return f_num;
+
+    return r_fib(f_num-1) + r_fib(f_num-2);
+}
+
+static PyObject*
+fibo_rec(PyObject* self,PyObject *args){
+     
+    long int fibo_number,result;
+
+    if (!PyArg_ParseTuple(args, "l", &fibo_number))
+             return NULL;
+
+    result = r_fib(fibo_number);
+    return Py_BuildValue("l",result);
+ 
+}
+
+
+
 static PyMethodDef fibo_methods[] = {
         //you list here the methods of that module
         {"iter_fibo", fibo_iter, METH_VARARGS},
+        {"rec_fibo", fibo_rec, METH_VARARGS},
         {NULL, NULL}
         };
 
