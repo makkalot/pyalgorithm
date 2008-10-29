20b91e5f2b3ad6f57050ef81ef8a704cf576cab9
makkalot <makkalot@acerfedora.(none)>
2008-10-29 12:47:52 +0200

    some util changes to make my life easier

--- util.py
+++ util.py
@@ -12,5 +12,21 @@ def generate_int(start,stop,num_of_elements):
 
     return random_list
 
+def generate_unique(start,stop,num_of_elements):
+    """
+    A little bit different for unique
+    """
+    random_list = []
+
+    while len(random_list)!=num_of_elements:
+        tmp = random.randint(start,stop)
+        if not tmp in random_list:
+            random_list.append(tmp)
+    return random_list
+
+
+
+
 if __name__ == "__main__":
-    print generate_int(1,100,20)
+    #print generate_int(1,100,20)
+    print generate_unique(1,10,9)
