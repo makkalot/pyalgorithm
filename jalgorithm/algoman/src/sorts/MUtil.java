3d6772ceee533add88a09cee3fcb8e82304223c5
makkalot <makkalot@fedorabig.localdomain>
2009-04-29 23:08:25 +0300

    adding util methods for sorts to generate arrays

--- jalgorithm/algoman/src/sorts/MUtil.java
+++ jalgorithm/algoman/src/sorts/MUtil.java
@@ -1,5 +1,7 @@
 package sorts;
 
+import java.util.Random;
+
 public class MUtil {
 	
 	public static int[] swap_items(int first,int second){
@@ -27,6 +29,37 @@ public class MUtil {
 		int tmp = array[first];
 		array[first] = array[second];
 		array[second] = tmp;
+		
+	}
+	
+	public static int[] generate_array(int how_many,int start,int end){
+		
+		int [] final_list = new int[how_many];
+		Random r = new Random();
+		
+		for(int i = 0;i<how_many;i++){
+			
+			long range = (long)end - (long)start + 1;
+		    // compute a fraction of the range, 0 <= frac < range
+		    long fraction = (long)(range * r.nextDouble());
+		    int randomNumber =  (int)(fraction + start);
+		    //add it to the end
+		    final_list[i]=randomNumber;
+		    
+			
+		}
+		return final_list;
+		
+	}
+	
+	
+	
+	public static void main(String[] argv){
+		
+		int[] mylist;
+		mylist = MUtil.generate_array(20, 100, 200);
+		MUtil.print_array(mylist);
+		
 		
 	}
 
